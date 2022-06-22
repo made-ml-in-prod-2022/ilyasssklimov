1318637ebbf87d162b1ec363b9bede0679f88b28
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-06-23 00:13:07 +0300

    add standard scaler to preprocess

--- airflow_ml_dags/images/airflow-preprocess/preprocess.py
+++ airflow_ml_dags/images/airflow-preprocess/preprocess.py
@@ -7,6 +7,8 @@ import os
 def transform_data(data: pandas.DataFrame) -> pandas.DataFrame:
     scaler = StandardScaler()
     transformed_data = scaler.fit_transform(data)
+    data_frame = pandas.DataFrame(transformed_data, columns=data.columns)
+    return data_frame
 
 
 @click.command('preprocess_data')
@@ -15,10 +17,11 @@ def transform_data(data: pandas.DataFrame) -> pandas.DataFrame:
 def preprocess_data(input_dir: str, output_dir: str) -> None:
     data = pandas.read_csv(os.path.join(input_dir, 'data.csv'))
     target = pandas.read_csv(os.path.join(input_dir, 'target.csv'))
+    transformed_data = transform_data(data)
 
     os.makedirs(output_dir, exist_ok=True)
-    data.to_csv(os.path.join(output_dir, 'data.csv'))
-    target.to_csv(os.path.join(output_dir, 'target.csv'))
+    transformed_data.to_csv(os.path.join(output_dir, 'data.csv'), index=False)
+    target.to_csv(os.path.join(output_dir, 'target.csv'), index=False)
 
 
 if __name__ == '__main__':
