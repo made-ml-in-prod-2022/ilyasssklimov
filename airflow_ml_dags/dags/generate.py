e988ad473b0173cf6e915298a811d5ca8e1349b4
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-06-23 00:11:24 +0300

    add train dag with preprocess task

--- airflow_ml_dags/dags/generate.py
+++ airflow_ml_dags/dags/generate.py
@@ -1,43 +1,29 @@
 from airflow import DAG
-from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.operators.empty import EmptyOperator
-from datetime import timedelta
 from docker.types import Mount
 from pendulum import today
-
-
-default_args = {
-    'owner': 'airflow',
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
-}
+import config as cfg
 
 
 with DAG(
         'data_generate',
-        default_args=default_args,
+        default_args=cfg.default_args,
         schedule_interval='@daily',
         start_date=today('UTC').add(days=-7)
 ) as dag:
-    start = EmptyOperator(task_id='start_data_acquisition')
+    start = EmptyOperator(task_id='start_data_generate')
 
     generate = DockerOperator(
         image='airflow-generate',
         task_id='data_generate',
-        command='--output-dir=/data/raw/{{ ds }}',
+        command=f'--output-dir={cfg.DATA_FOLDER}',
         network_mode='bridge',
         do_xcom_push=False,
         mount_tmp_dir=False,
-        mounts=[
-            Mount(
-                source=Variable.get('HOST_FOLDER'),
-                target='/data',
-                type='bind'
-            )
-        ]
+        mounts=[Mount(source=cfg.HOST_FOLDER, target='/data', type='bind')]
     )
 
-    end = EmptyOperator(task_id='end_data_acquisition')
+    end = EmptyOperator(task_id='end_data_generate')
 
     start >> generate >> end
---
+++ airflow_ml_dags/dags/train.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.sensors.filesystem import FileSensor
+from docker.types import Mount
+import os
+from pendulum import today
+import config as cfg
+
+
+with DAG(
+        'data_train',
+        default_args=cfg.default_args,
+        schedule_interval='@daily',
+        start_date=today('UTC').add(days=-7)
+) as dag:
+    start = EmptyOperator(task_id='start_data_train')
+
+    data_wait = FileSensor(
+        filepath=os.path.join(cfg.FULL_DATA_FOLDER, 'data.csv'),
+        poke_interval=60,
+        timeout=600,
+        mode='poke',
+        task_id='wait_for_data'
+    )
+    target_wait = FileSensor(
+        filepath=os.path.join(cfg.FULL_DATA_FOLDER, 'target.csv'),
+        poke_interval=60,
+        timeout=600,
+        mode='poke',
+        task_id='wait_for_target')
+
+    preprocess = DockerOperator(
+        image='airflow-preprocess',
+        command=f'--input-dir={cfg.DATA_FOLDER} --output-dir={cfg.PREPROCESS_FOLDER}',
+        task_id='data_preprocess',
+        do_xcom_push=False,
+        mount_tmp_dir=False,
+        mounts=[Mount(source=cfg.HOST_FOLDER, target='/data', type='bind')]
+    )
+
+    end = EmptyOperator(task_id='end_data_train')
+
+    start >> [data_wait, target_wait] >> preprocess >> end
