6f48e2b17bbca0b9d8a7e67023ef687cb399f3f4
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-06-23 00:10:53 +0300

    add config file for dags

---
+++ airflow_ml_dags/dags/config.py
@@ -0,0 +1,14 @@
+from airflow.models import Variable
+from datetime import timedelta
+
+
+DATA_FOLDER = '/data/raw/{{ ds }}'
+PREPROCESS_FOLDER = '/data/processed/{{ ds }}'
+HOST_FOLDER = Variable.get('HOST_FOLDER')
+FULL_DATA_FOLDER = '/opt/airflow' + DATA_FOLDER
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
